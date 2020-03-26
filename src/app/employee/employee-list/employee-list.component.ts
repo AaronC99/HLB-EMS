8fbdbf2b486f2818546912a99aa14b88d338ecd9
AaronCHE <aaronchuah99@gmail.com>
2020-03-26 13:31:17 +0800

    [Employee] Added dummy data in tables

--- src/app/employee/employee-list/employee-list.component.ts
+++ src/app/employee/employee-list/employee-list.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 
+export interface PeriodicElement {
+  name: string;
+  domainId: string;
+  email: string;
+}
+const ELEMENT_DATA: PeriodicElement[] = [
+  {name: 'Aaron Chuah', domainId: 'taaronchuah', email: 'taaronchuah@internal.hlb'},
+  
+];
 @Component({
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
   styleUrls: ['./employee-list.component.scss']
 })
 export class EmployeeListComponent implements OnInit {
-
+  displayedColumns: string[] = ['name', 'domainId', 'email'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
   constructor() { }
 
   ngOnInit(): void {
+  }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
 }
--- src/app/employee/timesheet/timesheet.component.ts
+++ src/app/employee/timesheet/timesheet.component.ts
@@ -3,23 +3,16 @@ import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms'
 import * as XLSX from 'xlsx';
 
 export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  date: string;
+  clockIn: string;
+  clockOut: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {date: '20/3/20', clockIn: '8:45', clockOut: '17:45'},
+  {date: '21/3/20', clockIn: '8:45', clockOut: '17:45'},
+  {date: '22/3/20', clockIn: '8:45', clockOut: '17:45'},
+  {date: '23/3/20', clockIn: '8:45', clockOut: '17:45'},
 ];
 @Component({
   selector: 'app-timesheet',
@@ -34,7 +27,7 @@ export class TimesheetComponent implements OnInit {
     selectedMonth: new FormControl(''),
     selectedYear: new FormControl('')
   });
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['date','clockIn','clockOut'];
   dataSource = ELEMENT_DATA;
   visible = false;
   constructor(private formBuilder:FormBuilder) { 
