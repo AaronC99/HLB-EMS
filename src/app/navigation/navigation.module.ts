a92bf2c3d49ef250eaede572ed54e60a28e81f0b
AaronCHE <aaronchuah99@gmail.com>
2020-04-03 11:08:51 +0800

    [PR 225] Remove unused modules in navigation.module.ts

--- src/app/navigation/navigation.module.ts
+++ src/app/navigation/navigation.module.ts
@@ -3,19 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { NavigationRoutingModule } from './navigation-routing.module';
 import { NavigationComponent } from './navigation.component';
-import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-import { MatInputModule } from '@angular/material/input';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSidenavModule } from '@angular/material/sidenav';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatListModule } from '@angular/material/list';
-import { MatTableModule } from '@angular/material/table';
-import { MatSelectModule } from '@angular/material/select';
-import { MatOptionModule } from '@angular/material/core';
 import { HeaderComponent } from './header/header.component';
 import { AuthenticationModule } from '../authentication/module/authentication.module';
 
@@ -29,19 +20,10 @@ import { AuthenticationModule } from '../authentication/module/authentication.mo
     CommonModule,
     AuthenticationModule,
     NavigationRoutingModule,
-    MatCardModule,
     MatButtonModule,
-    MatInputModule,
-    ReactiveFormsModule,
-    MatFormFieldModule,
     MatIconModule,
     MatToolbarModule,
-    MatSidenavModule,
     FlexLayoutModule,
-    MatListModule,
-    MatTableModule,
-    MatSelectModule,
-    MatOptionModule,
   ],
   providers: [],
   exports: [
