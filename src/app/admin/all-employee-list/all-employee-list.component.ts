6767feefb09da4ab2d181ab7abe06530a4fadedf
AaronCHE <aaronchuah99@gmail.com>
2020-04-07 21:50:59 +0800

    [Admin] Added Data Table to view all employee

--- src/app/admin/all-employee-list/all-employee-list.component.ts
+++ src/app/admin/all-employee-list/all-employee-list.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { AuthenticationService } from 'src/app/authentication/service/authentication.service';
+import { EmployeeService } from 'src/app/employee/service/employee.service';
 
+export interface AllEmployeeRecords {
+  name: string;
+  domainId: string;
+  email: string;
+  role: string;
+  scheduleName: string;
+  departmentName: string;
+}
+const EMPLOYEE_DATA: AllEmployeeRecords[] = [];
 @Component({
   selector: 'app-all-employee-list',
   templateUrl: './all-employee-list.component.html',
   styleUrls: ['./all-employee-list.component.scss']
 })
 export class AllEmployeeListComponent implements OnInit {
-
-  constructor() { }
+  displayedColumns: string[] = ['name', 'domainId', 'email','role','schedule','department'];
+  dataSource:any = new MatTableDataSource(EMPLOYEE_DATA);
+  currentUserId: String;
+  constructor(
+    private authService: AuthenticationService,
+    private employeeService: EmployeeService
+  ) {
+    this.authService.userAuthDetails.subscribe(userId => {
+      this.currentUserId = userId.username;
+    });
+   }
 
   ngOnInit(): void {
+    this.employeeService.getAllEmployees(this.currentUserId).subscribe(data=>{
+      this.dataSource = data;
+    });
+    this.dataSource.filterPredicate = function(data,filter): boolean{
+      return data.domainId.toLowerCase().includes(filter);
+    };
   }
 
+  applyFilter(event:string) {
+    //const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = event.trim().toLowerCase();
+  }
 }
