b03ece2d86a881d10f46534404025034f3deafbd
AaronCHE <aaronchuah99@gmail.com>
2020-04-08 20:03:02 +0800

    [Admin] Admn able to sort and filter. A pop up confirmation will appear when admin wants to deactivate an account

--- src/app/admin/all-employee-list/all-employee-list.component.ts
+++ src/app/admin/all-employee-list/all-employee-list.component.ts
@@ -3,30 +3,20 @@ import { MatTableDataSource } from '@angular/material/table';
 import { AuthenticationService } from 'src/app/authentication/service/authentication.service';
 import { EmployeeService } from 'src/app/employee/service/employee.service';
 import { MatSort } from '@angular/material/sort';
-import { trigger, state, style, transition, animate } from '@angular/animations';
-import { Employee } from 'src/app/model/Employee.model';
 import { AdminService } from '../service/admin.service';
 
 @Component({
   selector: 'app-all-employee-list',
   templateUrl: './all-employee-list.component.html',
   styleUrls: ['./all-employee-list.component.scss'],
-  // animations: [
-  //   trigger('detailExpand', [
-  //     state('collapsed', style({height: '0px', minHeight: '0'})),
-  //     state('expanded', style({height: '*'})),
-  //     transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
-  //   ]),
-  // ],
 })
-export class AllEmployeeListComponent implements OnInit,AfterViewInit {
+export class AllEmployeeListComponent implements OnInit {
   displayedColumns: string[] = ['name', 'domainId', 'email','role','schedule','department','edit','status'];
+  ELEMENT_DATA: any = [];
   dataSource:any = new MatTableDataSource<any>();
   currentUserId: String;
   expandedRow:number;
-  @ViewChild(MatSort, {static: true}) set matSort(sort: MatSort){
-    this.dataSource.sort = sort;
-  }
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
   constructor(
     private authService: AuthenticationService,
     private employeeService: EmployeeService,
@@ -39,29 +29,29 @@ export class AllEmployeeListComponent implements OnInit,AfterViewInit {
   
   ngOnInit(): void {
     this.getAllEmployeeDetails();
-    //this.dataSource.sort = this.sort;
-  }
-
-  ngAfterViewInit():void{
-    //this.dataSource.sort = this.sort;
   }
 
   public getAllEmployeeDetails(){
-    this.employeeService.getAllEmployees(this.currentUserId).subscribe(data=>{
-      this.dataSource = data;
+    this.employeeService.getAllEmployees(this.currentUserId).subscribe( data =>{
+      this.ELEMENT_DATA = data;
+      this.dataSource = new MatTableDataSource<any>(this.ELEMENT_DATA);
+      this.dataSource.sort = this.sort;
     });
   }
 
-  applyFilter(filterValue:string) {
+  applyFilter(event:Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLocaleLowerCase();
-    console.log(this.dataSource);
   }
 
   public deactivateAccount(currUser:any){
-    this.adminService.updateEmployee(currUser.domain_id,{activated:false});
+    if (confirm("Confirmation Message: \nDo you want to deactive "+ currUser.domain_id+" ? ")){
+      this.adminService.updateEmployee(currUser.domain_id,{activated:false});
+    } 
   }
 
   public editProfileDetails(currUser:any){
+  
     console.log("Edit User: " + currUser.name);
   }
 }
