9917c669a3cc28e3879656029b70ee8fc1fd8f6e
AaronCHE <aaronchuah99@gmail.com>
2020-05-21 13:24:36 +0800

    [Schedule] Provide data table to display all schedules

--- src/app/maintenance/all-schedule/all-schedule.component.ts
+++ src/app/maintenance/all-schedule/all-schedule.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSlideToggleChange } from '@angular/material/slide-toggle';
+import { AdminService } from 'src/app/admin/service/admin.service';
+import { Schedule } from 'src/app/model/Schedule.model';
 
 @Component({
   selector: 'app-all-schedule',
@@ -6,10 +11,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./all-schedule.component.scss']
 })
 export class AllScheduleComponent implements OnInit {
+  displayedColumns = ['no','scheduleName','daysOfWork','startTime','endTime','edit','status'];
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+  dataSource:any = new MatTableDataSource<any>();
+  checked: boolean;
+  allSchedules: Schedule[] = [];
+  activeSchedules: Schedule[] = [];
 
-  constructor() { }
+  constructor(
+    private adminService: AdminService
+  ) {
+   }
 
   ngOnInit(): void {
+    this.displayAllSkd();
+  }
+
+  public showAll($event: MatSlideToggleChange){
+    this.checked = $event.checked;
+    this.displayAllSkd();
+  }
+
+  public displayAllSkd(){
+    this.adminService.getAllSchedules().subscribe((data:Schedule[]) => {
+      this.allSchedules = data;
+      console.log(this.allSchedules);
+      if(this.checked){
+        // Show All
+        this.dataSource = new MatTableDataSource<any>(this.allSchedules);
+      } else {
+        // Show Only Active 
+        this.activeSchedules = this.allSchedules.filter(dept => dept['activated'] === true);
+        this.dataSource = new MatTableDataSource<any>(this.activeSchedules);
+      }
+      this.dataSource.sort = this.sort;
+    });
+  }
+
+  public editSkdDetails(schedule){
+    console.log(schedule);
+  }
+
+  public updateSkdStatus(schedule){
+    console.log(schedule);
   }
 
 }
